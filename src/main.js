26cbd13bb463604fad460b744d4c8c50d21961e8
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-05-19 11:49:14 -0300

    commit initial

---
+++ src/main.js
@@ -0,0 +1,98 @@
+/** Classe com método contrutor
+ * class TodoList{
+    constructor(){
+        this.todos = [];
+    }
+
+    addTodo(){
+        this.todos.push('Novo todo');
+        console.log(this.todos)
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.addTodo();
+}
+*/
+/** Classe com método estático 
+class Matematica{
+    static soma(a,b){
+        return a+b;
+    }
+}
+
+console.log(Matematica.soma(2,3));
+*/
+
+/** 
+const usuario = {nome: 'Alef'};
+
+usuario.nome = 'Diego';
+
+console.log(usuario);
+*/
+
+/**
+function teste(x){
+    let y =2;
+
+    if(x > 5){
+        console.log(x,y);
+    }
+}
+teste(10);*/
+/** 
+const arr = [1,3,4,5,8,9];
+
+const newArray = arr.map(function(item,index){
+    return item + index ;
+});
+
+console.log(arr);
+console.log(newArray);
+
+ const sum = arr.reduce(function(total, next){
+    return total + next;
+
+ });
+
+ console.log(sum);
+
+ const filter = arr.filter(function(item){
+    return item % 2 === 0;
+ });
+
+ console.log(filter);
+ */
+
+/** 
+const arr = [1,2,3,4,5,6,7,8,9,0];
+
+const newArr = arr.map(item => item * 2);
+
+console.log(arr,newArr);
+
+const soma = (a=2,b=3) => a+b;
+
+console.log(soma(2,8));
+
+*/
+
+const minhaPromise = () => new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve('OK')
+    }, 2000);
+});
+/** 
+minhaPromise().then(response => {
+    console.log(response);
+});*/
+
+async function executaPromise (){
+    const response = await minhaPromise();
+    console.log(response);
+}
+
+executaPromise();
